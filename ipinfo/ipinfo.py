affcbc20315967c8d69216bacf46252b10f2b970
kris <d4rkn3bula@pm.me>
2020-12-13 12:42:12 -0500

    Updated script to use the argparse module, now you can select which option to run.

--- ipinfo/ipinfo.py
+++ ipinfo/ipinfo.py
@@ -14,12 +14,23 @@ Keyring reference: https://pypi.org/project/keyring/
 import requests
 import json
 import keyring
-import sys
 import re
+import argparse
 
+# argparse function
+def get_args():
+
+	parser = argparse.ArgumentParser(prog='ipinfo lookup script', description='returns information of a given ip address')
+	parser.add_argument('-g', '--geolocate', help='Input IPv4 address to run the geolocation function')
+	parser.add_argument('-r', '--reverseip', help='Input IPv4 address to run the reverse ip check function')
+
+	args = parser.parse_args()
+
+	return args
 
 # check ip using regex, kill process if not
 def is_ip(ip):
+
 	ip_re = re.compile("^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$")
 	if ip_re.match(ip):
 		return ip
@@ -29,30 +40,41 @@ def is_ip(ip):
 		exit()
 
 # ipinfo IP geolocation API function
-def ipinfo_geolocation(ip):
+def ipinfo_geolocation(geo_ip):
+
 	token = keyring.get_password("ipinfo", "username")
 	base_url = "https://ipinfo.io/"
 	uri = "?token="
-	request = requests.get(base_url + ip + uri + token)
+	request = requests.get(base_url + geo_ip + uri + token)
 	data = json.loads(request.text)
 	print(json.dumps(data, indent =4, sort_keys=True))
 
 # ipinfo reverse IP API function
-def ipinfo_reverseIP(ip):
+def ipinfo_reverseIP(r_ip):
+
 	token = keyring.get_password("ipinfo", "username")
 	base_url = "https://ipinfo.io/domains/"
 	uri = "?token="
-	request = requests.get(base_url + ip + uri + token)
+	request = requests.get(base_url + r_ip + uri + token)
 	data = json.loads(request.text)
 	print(json.dumps(data, indent =4, sort_keys=True))
 
 
 # main function
 def main():
-	ip = sys.argv[1]
-	is_ip(ip)
-	ipinfo_geolocation(ip)
-	ipinfo_reverseIP(ip)
+
+	args = get_args()
+	geo_ip = args.geolocate
+	r_ip = args.reverseip
+
+	if args.geolocate:
+		is_ip(geo_ip)
+		ipinfo_geolocation(geo_ip)
+	elif args.reverseip:
+		is_ip(r_ip)
+		ipinfo_reverseIP(r_ip)
+	else:
+		print("something is messed up")
 
 
 if __name__ == "__main__":
